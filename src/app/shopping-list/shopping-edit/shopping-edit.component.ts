ae0ba8cdb7bde46df1e4465496f1cecb342ff0c2
Per Lovén <per.loven@solidbeans.com>
2023-03-06 08:35:21 +0100

    Add ingredient via ShoppingListService

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Ingredient } from "../../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list.service";
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,12 +10,14 @@ import { Ingredient } from "../../shared/ingredient.model";
 export class ShoppingEditComponent {
   @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
   @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
-  @Output() ingredientAdded = new EventEmitter<Ingredient>()
+
+  constructor(private shoppingListService: ShoppingListService) {
+  }
 
   onAddIngredient() {
     const name = this.nameInputRef.nativeElement.value;
     const amount = this.amountInputRef.nativeElement.value
     console.log('Adding ingredient ' + name + " (" + amount + ")")
-    this.ingredientAdded.emit(new Ingredient(name, amount));
+    this.shoppingListService.addIngredient(new Ingredient(name, amount));
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,19 @@
-export class ShoppingListService {
+import { Ingredient } from "../shared/ingredient.model";
+import { EventEmitter } from "@angular/core";
 
+export class ShoppingListService {
+  ingredientAdded = new EventEmitter<Ingredient>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Banana', 1),
+    new Ingredient('Apple', 5)
+  ]
+
+  addIngredient(ingredient: Ingredient): void {
+    this.ingredients.push(ingredient);
+    this.ingredientAdded.emit(ingredient);
+  }
+
+  getIngredients(): Ingredient[] {
+    return this.ingredients.slice();
+  }
 }
