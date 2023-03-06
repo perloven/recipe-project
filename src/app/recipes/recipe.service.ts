377c7e0d6811f6fae8b4f3cdae1831889587f51b
Per Lovén <per.loven@solidbeans.com>
2023-03-06 08:50:07 +0100

    Nicer recipe details ingredient list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,7 +7,7 @@ export class RecipeService {
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', [new Ingredient('Test ingredient', 1)],
       'https://assets.epicurious.com/photos/61f423f29c9591f7270e22c6/5:4/w_4171,h_3337,c_limit/Bouillabaise_RECIPE_20220125_1776_V1_final.jpg'),
-    new Recipe('Banana bread', 'Moist and delicious', [new Ingredient('Banana', 2), new Ingredient('Flour', 3)],
+    new Recipe('Banana bread', 'Moist and delicious', [new Ingredient('Banana', 2), new Ingredient('Flour', 3), new Ingredient('Butter', 1)],
       'https://www.simplyrecipes.com/thmb/tR-5eHAZ3lgNR6Yvu3yxdHMNpk8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Easy-Banana-Bread-LEAD-2-2-63dd39af009945d58f5bf4c2ae8d6070.jpg')
   ];
 
