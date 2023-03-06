61ef525ebbf71a96596c3150a33689fc049d9868
Per Lovén <per.loven@solidbeans.com>
2023-03-06 17:20:49 +0100

    Display recipes by default

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from "@angular/router";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
-const routes: Routes = [
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/recipes', pathMatch: 'full'},
   { path: 'recipes', component: RecipesComponent},
   { path: 'shopping-list', component: ShoppingListComponent}
-]
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
