597c3c0919f0c05bf38d6a2a5fb6d93a85718d9a
Per Lovén <per.loven@solidbeans.com>
2023-03-06 08:53:26 +0100

    Add recipe ingredients to shopping list

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Recipe } from "../recipe.model";
+import { ShoppingListService } from "../../shopping-list/shopping-list.service";
+import { Ingredient } from "../../shared/ingredient.model";
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,4 +10,11 @@ import { Recipe } from "../recipe.model";
 })
 export class RecipeDetailComponent {
   @Input() recipe: Recipe;
+
+  constructor(private shoppingListService: ShoppingListService) {
+  }
+
+  onAddToShoppingList() {
+    this.recipe.ingredients.forEach((ingredient: Ingredient) => this.shoppingListService.addIngredient(ingredient));
+  }
 }
