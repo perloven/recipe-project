416edea94c416eb2d8d83cf5fbf459e0ef8427d3
Per Lovén <per.loven@solidbeans.com>
2023-03-09 16:00:30 +0100

    Update form input from selected shopping list item

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -3,6 +3,7 @@ import { Subject } from "rxjs";
 
 export class ShoppingListService {
   ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
   private ingredients: Ingredient[] = [
     new Ingredient('Banana', 1),
     new Ingredient('Apple', 5)
@@ -20,5 +21,9 @@ export class ShoppingListService {
 
   getIngredients(): Ingredient[] {
     return this.ingredients.slice();
+  }
+
+  getIngredient(index: number): Ingredient {
+    return this.ingredients[index];
   }
 }
