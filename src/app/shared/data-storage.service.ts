b9f20fc61217e6197d6c5ab5f955930aeba8d110
Per Lovén <per.loven@solidbeans.com>
2023-03-10 17:08:14 +0100

    Avoid undefined ingredients for loaded recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
+import { map } from "rxjs";
 
 @Injectable()
 export class DataStorageService {
@@ -21,6 +22,11 @@ export class DataStorageService {
   loadRecipes() {
     this.http
       .get<Recipe[]>(this.baseUrl)
+      .pipe(map(recipes => {
+        return recipes.map(recipe => {
+          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+        })
+      }))
       .subscribe(recipes => {
         this.recipeService.setRecipes(recipes);
       })
