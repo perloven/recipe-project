afbaf8c53b15710b83f3bf0d1a0d26a752091f02
Per Lovén <per.loven@solidbeans.com>
2023-03-27 15:53:26 +0200

    Move services to core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListService } from "./shopping-list/shopping-list.service";
 import { AppRoutingModule } from "./app-routing.module";
-import { ErrorPageComponent } from './error-page/error-page.component';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { RecipeService } from "./recipes/recipe.service";
-import { DataStorageService } from "./shared/data-storage.service";
-import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
-import { RecipesResolverService } from "./recipes/recipes-resolver.service";
+import { HttpClientModule } from "@angular/common/http";
 import { AuthComponent } from "./auth/auth.component";
-import { AuthService } from "./auth/auth.service";
-import { AuthInterceptorService } from "./auth/auth-interceptor.service";
-import { AuthGuard } from "./auth/auth.guard";
 import { RecipesModule } from "./recipes/recipes.module";
 import { ShoppingListModule } from "./shopping-list/shopping-list.module";
 import { SharedModule } from "./shared/shared.module";
+import { CoreModule } from "./core.module";
 
 @NgModule({
   declarations: [
@@ -34,16 +27,8 @@ import { SharedModule } from "./shared/shared.module";
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
-    SharedModule
-  ],
-  providers: [
-    ShoppingListService,
-    RecipeService,
-    DataStorageService,
-    RecipesResolverService,
-    AuthService,
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true },
-    AuthGuard
+    SharedModule,
+    CoreModule
   ],
   bootstrap: [AppComponent]
 })
